f955314b28330dd95a179e2adeba6953cc787bb9
TziviYaffe <leahy0135@gmail.com>
2023-10-10 16:16:31 +0300

    first commit

---
+++ dolse.js
@@ -0,0 +1,5 @@
+function changPic(id){
+    document.getElementById("ab").src=document.getElementById(id).src
+    document.getElementById("ab").heigh="300";
+    document.getElementById("ab").width="300";
+}
